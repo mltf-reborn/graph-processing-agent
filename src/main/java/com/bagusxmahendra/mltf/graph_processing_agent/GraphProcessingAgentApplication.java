85514f6725dc697dc5a54ab14e716e53111cbe90
Bagus <bagusmwicaksono@gmail.com>
2026-08-31 02:56:16 +0000

    Initialize

---
+++ src/main/java/com/bagusxmahendra/mltf/graph_processing_agent/GraphProcessingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.graph_processing_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphProcessingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphProcessingAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagusxmahendra/mltf/graph_processing_agent/GraphProcessingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.graph_processing_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphProcessingAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
